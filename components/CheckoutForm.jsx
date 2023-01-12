3e462d75dd41d7fc63e8bbd0bc439702bb2ab97c
jfigueroapcs <jfigueroaubv@gmail.com>
2023-01-11 23:16:32 -0400

    vercion oficial

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   PaymentElement,
+  LinkAuthenticationElement,
   useStripe,
   useElements
 } from "@stripe/react-stripe-js";
@@ -9,6 +10,7 @@ export default function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [email, setEmail] = React.useState('');
   const [message, setMessage] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
 
@@ -82,6 +84,10 @@ export default function CheckoutForm() {
 
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
+      <LinkAuthenticationElement
+        id="link-authentication-element"
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <PaymentElement id="payment-element" options={paymentElementOptions} />
       <button disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
